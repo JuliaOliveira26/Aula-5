3ace4a5c9637800211ab3157deb1859e943f23d9
Julia.Oliveira26 <julinhalves2006@gmail.com>
2023-05-05 11:58:13 -0300

    zx

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+  import { ref } from 'vue'
+
+  const novoItem = ref('')
+  const listaCompras = ref([ 'arroz', 'batata', 'feijão' ])
+  
+  function adicionar (index){
+    listaCompras.value.push(index, novoItem.value)
+    novoItem.value = ''
+  }
+</script>
+<template>
+  <input type="text" v-model="novoItem">
+  <button @click="adicionar">Adicionar</button>
+  <ul>
+    <li @v-for="(item,index) in listaCompras">{{ listaCompras }}</li>
+  </ul>
+</template>
